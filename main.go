8c00a4bc3dfae4d263ee0667140115c98dee1d0a
Sebastian Barrenechea <sebastian@barrenechea.cl>
2024-04-19 15:04:37 -0400

    Add handleSetFirmwareRequest function to handle firmware updates

--- main.go
+++ main.go
@@ -155,6 +155,8 @@ func handleSetRequest(w http.ResponseWriter, r *http.Request) interface{} {
 		handleSetResetNodeRequest(w, r)
 	case "usb":
 		handleSetUSBModeRequest(w, r)
+	case "firmware":
+		handleSetFirmwareRequest(w, r)
 	}
 
 	return setResponse()
@@ -176,6 +178,23 @@ func handleGetRequest(w http.ResponseWriter, r *http.Request) interface{} {
 		w.Write([]byte("hello world"))
 		return nil
 	}
+}
+
+func handleSetFirmwareRequest(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseMultipartForm(200 * 1024 * 1024) // 200 MB
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	file, header, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	log.Printf("Received firmware file: %s", header.Filename)
 }
 
 func handleSetPowerRequest(r *http.Request) {
